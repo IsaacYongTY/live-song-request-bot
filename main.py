5dc94da042ccfcfa0c821a5ee1d2172eadeafdc9
Isaac Yong <isaac.yongty@gmail.com>
2021-02-16 01:51:09 +0800

    Solved duplicate entries, set interval to 20

--- main.py
+++ main.py
@@ -43,76 +43,98 @@ def get_livechatid(token, url):
 
     return live_chat_id
 
-
+previous_page_token = ''
 next_page_token = ''
+bot_activate_interval = 20
 
 
-def get_latest_message(live_chat_id, page_token):
+class SongRequest:
+    def __init__(self, author_id, song_title):
+        self.author_id = author_id
+        self.song_title = song_title
+        self.name = None
 
+
+def get_latest_message(live_chat_id):
+
+    global next_page_token
     youtube = token
 
     request = youtube.liveChatMessages().list(
         liveChatId=live_chat_id,
         part="snippet",
-        pageToken=page_token
+        pageToken=next_page_token
     )
 
-    response = request.execute()
+    live_chat_response = request.execute()
 
-    # latest_message = response['items'][-1]
-    # latest_message_word_list = latest_message['snippet']['textMessageDetails']['messageText'].split(' ', 1)
-
-    messages_list = response['items']
+    messages_list = live_chat_response['items']
     channel_id_list = []
-    song_title_list = []
-    author_name_list = []
-    global next_page_token
-    next_page_token = response['items']['nextPageToken']
+    song_request_list = []
 
+
+
+
+    print(next_page_token)
+
+    print(messages_list)
     for message in messages_list:
 
         word_list = message['snippet']['textMessageDetails']['messageText'].split(' ', 1)
         command = word_list[0]
 
-        if command == '!request' && len(word_list) > 1:
+        if command == '!request' and len(word_list) > 1:
             song_title = word_list[1]
-            print('this is a command')
 
-            channel_id_list.append(response['items']['snippet']['authorChannelId'])
-            song_title_list.append(song_title)
+            song_request = SongRequest(message['snippet']['authorChannelId'], song_title)
+            song_request_list.append(song_request)
 
-    request = youtube.channels().list(
-        part='snippet',
-        id=','.join(channel_id_list)
-    )
+    for song_request in song_request_list:
+        channel_id_list.append(song_request.author_id)
 
-    response = request.execute()
+    if len(song_request_list) > 0:
+        request = youtube.channels().list(
+            part='snippet',
+            id=','.join(channel_id_list)
+        )
 
-    for item in response['items']:
-        author_name_list.append(item['snippet']['title'])
+        channels_response = request.execute()
 
-    if len(word_list) > 1:
-        print(f'{song_title} is requested')
+        for song_request in song_request_list:
+            for item in channels_response['items']:
+                if item['id'] == song_request.author_id:
+                    song_request.name = item['snippet']['title']
 
-        write_to_csv(song_title_list, author_name_list)
+        if len(word_list) > 1:
+            for song_request in song_request_list:
+                print(f'{song_request.song_title} is requested')
+
+            write_to_csv(song_request_list)
+
+    else:
+
+        print('working but empty')
+
+    next_page_token = live_chat_response['nextPageToken']
 
 
 def set_interval(func, sec):
-    def func_wrapper(live_chat_id,next_page_token):
+    def func_wrapper(live_chat_id):
         set_interval(func,sec)
-        func(live_chat_id, next_page_token)
+        func(live_chat_id)
 
-    t = threading.Timer(sec, func_wrapper, args=[live_chat_id, next_page_token])
+    t = threading.Timer(sec, func_wrapper, args=[live_chat_id])
     t.start()
     return t
 
 
-def write_to_csv(song_title_list, author_name_list):
+def write_to_csv(song_request_list):
     with open('./song_request.csv', 'a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter=',')
 
-        for song_title,author in zip(song_title_list,author_name_list):
-            writer.writerow(song_title, author, 1)
+        for song_request in song_request_list:
+            writer.writerow([song_request.song_title, song_request.name, 1])
+
 
 if __name__ == '__main__':
 
@@ -120,4 +142,4 @@ if __name__ == '__main__':
     url = 'https://youtu.be/I_Yv5DCivw0'
     live_chat_id = get_livechatid(token, url)
 
-    set_interval(get_latest_message,30)
+    set_interval(get_latest_message, bot_activate_interval)
